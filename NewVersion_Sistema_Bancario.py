7b42d79193bc6bcfea559a3361e610b804ac0ccf
Horst <arielhorst@gmail.com>
2023-06-06 17:01:01 -0300

    First settings

--- NewVersion_Sistema_Bancario.py
+++ NewVersion_Sistema_Bancario.py
@@ -1,1 +1,24 @@
-#Entendo
+                                        # Nova versão do SISTEMA BANCÁRIO 
+# Início do Script:
+
+# - Importanto pacotes:
+from datetime import datetime,timedelta
+
+# - Criando/Inicionando variáveis e listas:
+present_day = datetime.now()
+tomorrow = present_day + timedelta(1)
+depositos_list = []
+saques_list = []
+temp_dict = {}
+value_saldo = 0.0
+value_dep = 0.0
+numbers_saque = 1
+
+
+
+
+
+
+
+
+def menu():
